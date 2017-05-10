f68d325d35cf6cfc108d2cb096474aa41db59806
bitsnowwolf <bitsnowwolf@163.com>
2017-05-10 20:14:25 +0800

    add hello

---
+++ Hello.java
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: Hello.java
+	> Author:Bit 
+	> Mail:bitsnowwolf@163.com 
+	> Created Time: 2017年05月10日 星期三 20时09分46秒
+ ************************************************************************/
+
+public class Hello
+{
+    public static void main(String[] args){
+        System.out.print("hello");
+    }
+}
+
